45b74d741e069a0c27d39b912e4214adca6c68d9
Yuali28 <robiyuali285@gmail.com>
2023-07-21 03:56:06 +0800

    Fix SIBM

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -166,7 +166,7 @@
 
           </div>
 
-          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/why-us.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/sibm.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
         </div>
 
       </div>
